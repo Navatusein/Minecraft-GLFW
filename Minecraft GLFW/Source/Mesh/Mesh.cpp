851389b82f4ea17cf3876ed5d85cf98625cbec2f
cmbcoldspot <cnbcoldcpot@gmail.com>
2020-11-01 19:05:13 +0200

    Mesh class

---
+++ Minecraft GLFW/Source/Mesh/Mesh.cpp
@@ -0,0 +1,21 @@
+#include "Mesh.h"
+#include <iostream>
+
+Mesh::Mesh(float* vertices, unsigned int vCount) : vCount(vCount){
+	VBO.Construct(vertices, vCount);
+	std::cout << "jack here\n";
+	VAO.AddBuffer(VBO);
+	
+	VAO.Unbind();
+	VBO.Unbind();
+}
+
+Mesh::~Mesh() {
+}
+
+void Mesh::Draw() {
+	VAO.Bind();
+	
+	glDrawArrays(GL_TRIANGLES, 0, vCount);
+	VAO.Unbind();
+}
--- Minecraft GLFW/Source/Vertex/VertexBuffer.cpp
+++ Minecraft GLFW/Source/Vertex/VertexBuffer.cpp
@@ -1,4 +1,5 @@
 #include <GL/glew.h>
+#include <iostream>;
 
 #include "VertexBuffer.h"
 #include "../ErrorHandling.h"
@@ -9,8 +10,17 @@ VertexBuffer::VertexBuffer(float* data, unsigned int vertexCount) : mID(0) {
 	GLCall(glBufferData(GL_ARRAY_BUFFER, 5 * (int)vertexCount * sizeof(float) , data, GL_STATIC_DRAW));
 }
 
+VertexBuffer::VertexBuffer() : mID(0){
+}
+
 VertexBuffer::~VertexBuffer() {
 	GLCall(glDeleteBuffers(1, &mID));
+}
+
+void VertexBuffer::Construct(float* data, unsigned int vertexCount) {
+	GLCall(glGenBuffers(1, &mID));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, mID));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 5 * (int)vertexCount * sizeof(float), data, GL_STATIC_DRAW));
 }
 
 void VertexBuffer::Bind() {
